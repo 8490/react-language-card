e5ad64f92dd9a2cb7f22929bac1de919bb154d81
8490 <ethan@clarusway.com>
2022-03-21 22:47:06 +0300

    update components

--- src/App.js
+++ src/App.js
@@ -1,30 +1,26 @@
 import React from "react";
-import react from '../src/assets/react.svg';
-import './App.scss';
-import {categories} from "./helpers/data"
 
-import Card from "./card/Card"
+import react from "../src/assets/react.svg";
+import "./App.scss";
+import { categories } from "./helpers/data";
+import Card from "./card/Card";
 
 console.log(categories[0]);
 
 function App() {
   return (
-    <div className="App">
-      
+    <>
       <img src={react} className="App-logo" alt="logo" />
       <div className="container">
-          <div className="bars"></div>
-          <h1 className="header">Languages</h1>
-          <div className="card">
-            
-            {categories.map((category, index) => {
-              return <Card key={index} category={category} />
-            })}
-          </div>
-
+        <div className="bars"></div>
+        <h1 className="header">Languages</h1>
+        <div className="card">
+          {categories.map((category, index) => {
+            return <Card key={index} category={category} />;
+          })}
+        </div>
       </div>
-        
-    </div>
+    </>
   );
 }
 
